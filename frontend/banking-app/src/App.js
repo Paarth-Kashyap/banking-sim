614e7a88506fd13eddde8e9a852f9e759852e093
Paarth Kashyap <kashyap.paarth@gmail.com>
2024-09-24 16:52:23 -0400

    added template login

---
+++ frontend/banking-app/src/App.js
@@ -0,0 +1,19 @@
+import Login from './components/Login';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    <Login />
+      <h1>
+        Welcome to the Banking App
+      </h1>
+      
+     
+     
+    </div>
+  );
+}
+
+export default App;
